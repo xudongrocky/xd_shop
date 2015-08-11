3e9d935161e5915aaf6a022d282f0485b1edb8d6
xudongrocky <xudongrocky@qq.com>
2015-08-11 15:53:49 +0800

    add t1

---
+++ test1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xudong
+ * Date: 15/8/4
+ * Time: 下午6:23
+ */
+
+echo md5("king");
+$sql = "insert xd_admin(username,password,email) values ('admin','b2086154f101464aab3328ba7e060deb','348578347@qq.com')";
+echo $sql;
